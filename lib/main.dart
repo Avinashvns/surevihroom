4e9b15e8dd5a058690c776afe214fe728bcb178f
AVINASH SINGH <83054042+Avinashvns@users.noreply.github.com>
2023-12-07 12:17:28 +0530

    Navigation with named routes

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
+    // Navigation with named routes
     routes: {
       '/': (constext) => RoomRent(),
     },
